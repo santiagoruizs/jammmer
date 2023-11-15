9801053430d2e75eb73afa379f73c46796e21bbd
Santiago Ruiz <santiagoruiz@Air-de-Santiago.home>
2023-11-15 19:55:37 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Holy Shit
+        
         </a>
       </header>
     </div>
