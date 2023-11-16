b89800d3a8bc138c6e6479011c224afd9395f05a
Santiago Ruiz <santiagoruiz@Air-de-Santiago.home>
2023-11-16 18:44:11 +0100

    Styles fixes

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,7 +5,7 @@ import style from '../styles/Playlist.module.css'
 function PlayList (props) {
     return(
         <div className={style.container} >
-            <input type="text" className={style.playlistTitle} />  
+            <input type="text" className={style.playlistTitle} placeholder='Type Your Playlist Title' />  
             <TrackList songs={props.playlist} trackClickHandler={props.trackClick}/>
             <button className={style.button}>SAVE TO SPOTIFY</button>
         </div>
